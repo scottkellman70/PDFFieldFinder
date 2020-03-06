94574be76fc2d76928f180c9bc34d3eac4cf5871
Scott Kellman <scottkellman@hotmail.com>
2020-03-06 10:52:48 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,80 @@
+﻿namespace PDFFieldFinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ButtonGetFields = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // ButtonGetFields
+            // 
+            this.ButtonGetFields.Location = new System.Drawing.Point(12, 12);
+            this.ButtonGetFields.Name = "ButtonGetFields";
+            this.ButtonGetFields.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGetFields.TabIndex = 0;
+            this.ButtonGetFields.Text = "Get Fields";
+            this.ButtonGetFields.UseVisualStyleBackColor = true;
+            this.ButtonGetFields.Click += new System.EventHandler(this.ButtonGetFields_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(366, 15);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(422, 393);
+            this.textBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.ButtonGetFields);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ButtonGetFields;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDFFieldFinder
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        /// <summary>  
+        /// List all of the form fields into a textbox.  The  
+        /// form fields identified can be used to map each of the  
+        /// fields in a PDF.  
+        /// </summary>  
+        private void ListFieldNames()
+        {
+            DialogResult result = this.openFileDialog1.ShowDialog();
+
+            if (result == DialogResult.OK && this.openFileDialog1.CheckFileExists == true)
+            {
+                string pdfTemplate = this.openFileDialog1.FileName;
+
+                // title the form  
+                this.Text += " - " + pdfTemplate;
+
+                // create a new PDF reader based on the PDF template document  
+                PdfReader pdfReader = new PdfReader(pdfTemplate);
+
+                // create and populate a string builder with each of the field names available in the subject PDF  
+                StringBuilder sb = new StringBuilder();
+
+                foreach (KeyValuePair<string, AcroFields.Item> de in pdfReader.AcroFields.Fields)
+                {
+                    sb.Append(de.Key.ToString() + Environment.NewLine);
+                }
+
+                // Write the string builder's content to the form's textbox  
+                textBox1.Text = sb.ToString();
+                textBox1.SelectionStart = 0;
+            }
+
+        }
+        private void ButtonGetFields_Click(object sender, EventArgs e)
+        {
+            ListFieldNames();
+        }
+    }
+}
