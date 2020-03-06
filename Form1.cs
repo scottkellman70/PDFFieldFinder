dc92bface9fede21f625db5c4869d5ece1791594
Scott Kellman <scottkellman@hotmail.com>
2020-03-06 11:09:07 -0500

    remove and sort usings

--- Form1.cs
+++ Form1.cs
@@ -1,12 +1,7 @@
 ﻿using iTextSharp.text.pdf;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace PDFFieldFinder
